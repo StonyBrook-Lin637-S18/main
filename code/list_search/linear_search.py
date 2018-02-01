41618388b533ac385ec34010007bf6849af8fce6
Thomas Graf <git@thomasgraf.net>
2018-02-01 14:41:40 -0500

    add hypothesis tests to linear search

--- code/list_search/linear_search.py
+++ code/list_search/linear_search.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from hypothesis import given, settings
+import hypothesis.strategies as st
+
 
 def linear_search(search_list, item):
     """Search search_list from left to right for item.
@@ -25,6 +28,17 @@ def linear_search(search_list, item):
     False
     """
     for i in range(len(search_list)):
-        if search_item == search_list[i]:
+        if item == search_list[i]:
             return i
     return False
+
+
+@given(search_list=st.lists(st.integers()),
+       item=st.integers())
+@settings(max_examples=10000)
+def test_binary_search(search_list, item):
+    if item in search_list:
+        pos = linear_search(search_list, item)
+        assert search_list[pos] == item
+    else:
+        assert linear_search(search_list, item) is False
